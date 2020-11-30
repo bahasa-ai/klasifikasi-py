5c2808ea38f93b9a40738b821c561a1e7267e149
Restyled.io <commits@restyled.io>
2020-11-30 09:27:51 +0000

    Restyled by black

--- klasifikasi/klasifikasi.py
+++ klasifikasi/klasifikasi.py
@@ -4,18 +4,25 @@ from ._settings import BASE_URL
 
 class Klasifikasi:
     def __init__(self, creds, url=BASE_URL):
-        if (not isinstance(creds, list)):
+        if not isinstance(creds, list):
             raise Exception(
-                'Creds must be a list of dict contain client_id & client_secret')
+                "Creds must be a list of dict contain client_id & client_secret"
+            )
 
         self.__model_mapping = {}
         for data in creds:
-            if (not isinstance(data, dict) or data.get('client_id') is None or data.get('client_secret') is None):
+            if (
+                not isinstance(data, dict)
+                or data.get("client_id") is None
+                or data.get("client_secret") is None
+            ):
                 raise Exception(
-                    'Creds must be a list of dict contain client_id & client_secret')
+                    "Creds must be a list of dict contain client_id & client_secret"
+                )
 
-            self.__model_mapping[data.get('client_id')] = KlasifikasiModel(
-                data.get('client_id'), data.get('client_secret'), url)
+            self.__model_mapping[data.get("client_id")] = KlasifikasiModel(
+                data.get("client_id"), data.get("client_secret"), url
+            )
 
     def get_models(self):
         return self.__model_mapping
