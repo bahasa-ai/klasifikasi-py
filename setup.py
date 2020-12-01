4fe342029d3b1d9e7b733e482bd6bbec33c89370
zesagata <zezagata17@gmail.com>
2020-12-01 17:50:31 +0700

    change requirements.txt encoding, include requirements.txt at setup.py, bump version

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@ import setuptools
 with open('README.md', 'r') as fh:
     long_description = fh.read()
 
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name='klasifikasi-py', # Replace with your own username
-    version='0.0.1',
+    version='0.0.2',
     author='Z. E. Sagata',
     author_email='saga@bahasa.ai',
     description='Official Klasifikasi (https://klasifikasi.com) API Client Library',
@@ -19,4 +22,5 @@ setuptools.setup(
         'Operating System :: OS Independent',
     ],
     python_requires='>=3.7',
+    install_requires=required
 )
