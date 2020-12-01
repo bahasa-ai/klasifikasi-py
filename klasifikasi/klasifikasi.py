6d280df51b88179195a44781752565d272a3b136
zesagata <zezagata17@gmail.com>
2020-12-01 10:58:23 +0700

    implement logs function

--- klasifikasi/klasifikasi.py
+++ klasifikasi/klasifikasi.py
@@ -1,4 +1,5 @@
 import requests
+from dateutil import parser
 from ._klasifikasi_model import KlasifikasiModel
 from ._settings import BASE_URL
 
@@ -38,3 +39,43 @@ class Klasifikasi:
       raise Exception(error) 
     
     return response_json
+
+  def logs(self, public_id, query):
+    if (self.__model_mapping.get(public_id) is None):
+      raise Exception('Model not found !')
+    
+    model = self.__model_mapping.get(public_id)
+    
+    if query.get('started_at') is None or query.get('ended_at') is None:
+      raise Exception('Date filter is required')
+    
+    query_string = []
+    started_at = parser.parse(query.get('started_at'))
+    query_string.append('startedAt={}'.format(started_at.isoformat()))
+    
+    ended_at = parser.parse(query.get('ended_at'))
+    query_string.append('endedAt={}'.format(ended_at.isoformat()))
+    
+    if query.get('skip'):
+      query_string.append('skip={}'.format(query.get('skip')))
+    
+    if query.get('take'):
+      query_string.append('take={}'.format(query.get('take')))
+    
+    headers = { 'Authorization': 'Bearer {}'.format(model.get_token()) }
+    full_url = '{}/api/v1/history/{}?{}'.format(
+      self.url,
+      public_id,
+      '&'.join(query_string)
+    )
+    response = requests.get(full_url, headers = headers)
+    response_json = response.json()
+    
+    if response.status_code != 200:
+      error = 'Failed to classify into klasifikasi-api'
+      if response_json.get('error'):
+        error = response_json.get('error')
+      raise Exception(error) 
+    
+    return response_json
+    
