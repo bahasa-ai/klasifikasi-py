2cc599f4d75c554dcdf40bbef563fc6a146c1eb9
Restyled.io <commits@restyled.io>
2020-12-01 09:42:38 +0000

    Restyled by autopep8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='klasifikasi-py', # Replace with your own username
+    name='klasifikasi-py',  # Replace with your own username
     version='0.0.1',
     author='Z. E. Sagata',
     author_email='saga@bahasa.ai',
